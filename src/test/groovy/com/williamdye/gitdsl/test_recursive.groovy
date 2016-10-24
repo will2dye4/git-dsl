8fb97d41a5fac1ac5d42cd63d065451721163234
wdye <wdye@pindrop.com>
2016-10-24 17:53:56 -0400

    Added debug to recursive script.

--- src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy
+++ src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy
@@ -4,6 +4,7 @@ import static com.williamdye.gitdsl.Script.git
 
 
 git(args[0]) {
+    println "running, directory is $directory"
     clone "https://github.com/will2dye4/git-dsl.git"
     def classpath = "src/main/groovy"
     def script = "src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy"
