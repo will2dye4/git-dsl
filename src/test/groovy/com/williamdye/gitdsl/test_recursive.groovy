a07dba65235caf34e4f62e6484e3a1168dead8d4
wdye <wdye@pindrop.com>
2016-10-24 18:01:10 -0400

    Testing Thread.start

--- src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy
+++ src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy
@@ -6,8 +6,8 @@ import static com.williamdye.gitdsl.Script.git
 git(args[0]) {
     println "running, directory is $directory"
     clone "https://github.com/will2dye4/git-dsl.git"
-    Thread.sleep 1000
+    //Thread.sleep 1000
     def classpath = "src/main/groovy"
     def script = "src/test/groovy/com/williamdye/gitdsl/test_recursive.groovy"
-    execute "groovy -cp $classpath $script $directory"
+    Thread.start { -> execute "groovy -cp $classpath $script $directory" }
 }
